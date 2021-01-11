e3f5b538bfb9f98be8d4920e7d09efbe4568fae0
ransayada <46240258+ransayada@users.noreply.github.com>
2021-01-11 16:14:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -138,10 +138,15 @@ class _Repository:
     """)
 
     def receive_Shipment(self, name, amount, date):
+        self.logistics.incCountRecived(amount,self.suppliers.getSupId(name))
+        self.vaccines.insert(Vaccine( id,date, self.suppliers.getSupId(name), amount))
 
     def send_Shipment(self, location, amount):
+        self.clinics.reduceDemend(amount,location)
+        self.vaccines.removeAmount(amount)
+        self.logistics.incCountSent(amount,self.clinics.getSupId(location))
 
-    def action_log(self):
+    def action_log(self): # what is that ?
 
 
 # DAO
@@ -154,6 +159,22 @@ class _Vaccines:
         self._conn.execute("""
                INSERT INTO vaccines (id, date, supplier, quantity) VALUES (?, ?, ?, ?);""", [vaccine.id, vaccine.date, vaccine.supplier, vaccine.quantity])
 
+    #recursive method if amount > from the id 1 demand than remove the demand from amount and check all again
+    # if amount< the id 1 demand than remove the amount from the deman
+    def removeAmount(self,amount):
+        cursor = self._conn.execute("""
+                      SELECT column_name =demand FROM vaccines """)
+        curr_inventory = cursor.fetchone()[0]
+        if curr_inventory<amount:
+            self._conn.execute("""
+                           DELETE from vaccine where id = 1 """)
+            self.removeAmount(amount-curr_inventory)
+        update_inventory = curr_inventory-amount
+        self._conn.execute("""UPDATE vaccines SET demand = %d WHERE id=1 """ % update_inventory)
+        self._conn.commit()
+
+
+
 class _Suppliers:
     def __init__(self, conn):
         self._conn = conn
@@ -162,6 +183,12 @@ class _Suppliers:
         self._conn.execute("""
                 INSERT INTO suppliers (id, name, logistics) VALUES (?, ?, ?)
         """, [supplier.id, supplier.name, supplier.logistic])
+
+    #get the suplier's name id
+    def getSupId(self, name):
+        cursor = self._conn.execute("""
+                  SELECT logistics FROM suppliers WHERE name=%d""" % name)
+        return cursor.fetchone()[0]
 
 
 
@@ -174,6 +201,22 @@ class _Clinics:
             INSERT INTO clinics (id, location, demand, logistic) VALUES (?, ?, ?,?)
         """, [clinic.id, clinic.location, clinic.demand, clinic.logistic])
 
+    # get the suplier's location id
+    def getSupId(self,location):
+        cursor = self._conn.execute("""
+               SELECT logistics FROM clinics WHERE location=%d""" % location)
+        return cursor.fetchone()[0]
+
+    # reduce the amount from the location demand
+    def reduceDemend(self ,amount, location):
+        cursor = self._conn.execute("""
+                       SELECT demand FROM clinics WHERE location=%d""" % location)
+        self._conn.execute("""UPDATE clinucs SET demand = %s WHERE locatyin= %d """ % (cursor-amount, location))
+        self._conn.commit()
+
+
+
+
 
 
 class _Logistics:
@@ -184,3 +227,19 @@ class _Logistics:
         self._conn.execute("""
             INSERT INTO logistics (id, name, count_sent, count_received) VALUES (?, ?, ?)
         """, [logistic.id, logistic.name, logistic.count_sent, logistic.count_received])
+
+    #increase the count_recived/count_sent by amount
+    def incCountRecived(self, amount, logId):
+        cr = self._conn.execute('SELECT count_received FROM logistics WHERE id=%d' % logId)
+        logistic_id_cr = cr.fetchone()[0]
+        new_logistic_id_cr = logistic_id_cr + amount
+        self._conn.execute("""UPDATE logistics SET count_received = %s WHERE id= %d """ %(new_logistic_id_cr,logId))
+        self._conn.commit()
+    def incCountSent(self, amount, logId):
+        cr = self._conn.execute('SELECT count_sent FROM logistics WHERE id=%d' % logId)
+        logistic_id_cs = cr.fetchone()[0]
+        new_logistic_id_cs = logistic_id_cs + amount
+        self._conn.execute("""UPDATE logistics SET count_sent = %s WHERE id= %d """ % (new_logistic_id_cs, logId))
+        self._conn.commit()
+
+
